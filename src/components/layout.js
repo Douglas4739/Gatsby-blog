35a308a7eb6980bb4b3e5b94f536ea90bd34c22b
DouglasEde <briancarter340@gmail.com>
2022-04-16 21:06:21 -0700

    formatting

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,22 +1,29 @@
-import * as React from 'react'
-import { Link } from 'gatsby'
+import * as React from "react";
+import { Link } from "gatsby";
 import {
-    container,
-    heading,
-    navLinks,
-    navLinkItem,
-    navLinkText
-  } from './layout.module.css'
-
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText,
+} from "./layout.module.css";
 
 const Layout = ({ pageTitle, children }) => {
   return (
-    <div className={container}> 
+    <div className={container}>
       <title>{pageTitle}</title>
       <nav>
         <ul className={navLinks}>
-          <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
-          <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}>
+              Home
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/about" className={navLinkText}>
+              About
+            </Link>
+          </li>
         </ul>
       </nav>
       <main>
@@ -24,6 +31,6 @@ const Layout = ({ pageTitle, children }) => {
         {children}
       </main>
     </div>
-  )
-}
-export default Layout
+  );
+};
+export default Layout;
